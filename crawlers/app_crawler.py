d521475084f6067a865673b3379c16f93d08159c
root <root@duplt.localdomain>
2025-12-28 01:07:43 +0700

    Add content_crawler.py to collect app's content by ID

--- crawlers/app_crawler.py
+++ crawlers/app_crawler.py
@@ -5,37 +5,47 @@ import random
 import requests
 from tqdm import tqdm
 
-http_proxies = [line.strip() for line in open(file = "proxies.txt", mode = 'r').readlines()]
+# Define constants
+HTTP_PROXIES = [line.strip() for line in open(file = "proxies.txt", mode = 'r').readlines()]
 
+# Initialize S3 client
 s3 = boto3.client("s3")
 
+# Load S3 configurations
 s3_configs = yaml.safe_load(open(file = "configs/aws.yaml", mode = "r").read())
+
+# Define S3 directories
 tag_dir = {
     "bucket": s3_configs['s3']['bucket'],
-    "endpoint": f"{s3_configs['s3']['raw_prefix']}/{s3_configs['s3']['tag_endpoint']}",
+    "endpoint": f"{s3_configs['s3']['raw_prefix']}/{s3_configs['s3']['tag_endpoint']}"
 }
 app_dir = {
     "bucket": s3_configs['s3']['bucket'],
-    "endpoint": f"{s3_configs['s3']['raw_prefix']}/{s3_configs['s3']['app_endpoint']}",
+    "endpoint": f"{s3_configs['s3']['raw_prefix']}/{s3_configs['s3']['app_endpoint']}"
 }
 
-
+# Fetch tags from S3
 obj = s3.get_object(
     Bucket=tag_dir['bucket'],
     Key=f"{tag_dir['endpoint']}/tags.json"
 )
 tags = json.loads(obj['Body'].read().decode('utf-8'))
 
-url = "https://store.steampowered.com/saleaction/ajaxgetsaledynamicappquery"
+# For each tag, fetch associated appIDs and store to S3
 for tag in tags:
-
+    # Paginate through results
     last = False
     query = 0
     count = 5000
     
+    # Progress bar
     pbar = tqdm(desc=f"Tag {tag['tagid']}, Query", unit=" ")
     
+    # While not last page
     while not last:
+        
+        # Define request parameters
+        url = "https://store.steampowered.com/saleaction/ajaxgetsaledynamicappquery"
         params = {
             "cc": "VN",
             "l": "english",
@@ -48,13 +58,15 @@ for tag in tags:
             "bAllowDemos": False,
             "return_capsules": True
         }
-        http_proxy = random.choice(http_proxies)
+        http_proxy = random.choice(HTTP_PROXIES)
         response = requests.get(
             url = url, 
             proxies = {"http": http_proxy},
             params = params, 
             timeout = 10
         )
+        
+        # Process response
         if response.status_code == 200:
             tag_apps = response.json()
             
--- crawlers/tag_crawler.py
+++ crawlers/tag_crawler.py
@@ -4,24 +4,31 @@ import boto3
 import json
 import yaml
 
-http_proxies = [line.strip() for line in open(file = "proxies.txt", mode = 'r').readlines()]
+# Define constants
+HTTP_PROXIES = [line.strip() for line in open(file = "proxies.txt", mode = 'r').readlines()]
 
+# Initialize S3 client
 s3 = boto3.client("s3")
+
+# Load S3 configurations
 s3_configs = yaml.safe_load(open(file = "configs/aws.yaml", mode = "r").read())
+
+# Define S3 directories
 tag_dir = {
     "bucket": s3_configs['s3']['bucket'],
     "endpoint": f"{s3_configs['s3']['raw_prefix']}/{s3_configs['s3']['tag_endpoint']}",
 }
 
+# Fetch tags from Steam API
 url = "https://store.steampowered.com/actions/ajaxgetstoretags"
-http_proxy = random.choice(http_proxies)
+http_proxy = random.choice(HTTP_PROXIES)
 response = requests.get(
     url = url,
     proxies={"http": http_proxy}, timeout=10
 )
 tags = response.json()['tags']
 
-
+# Store tags to S3
 s3.put_object(
     Bucket=tag_dir['bucket'],
     Key=f"{tag_dir['endpoint']}/tags.json",
