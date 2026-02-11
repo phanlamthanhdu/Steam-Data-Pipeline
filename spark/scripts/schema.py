adb2548ff37dd2efe679f79aadc4b47fbcd3979e
phanlamthanhdu <root@duplt.localdomain>
2026-02-11 22:13:26 +0700

    Modify schema for content, review, tag

--- spark/scripts/schema.py
+++ spark/scripts/schema.py
@@ -10,28 +10,28 @@ from pyspark.sql.types import (IntegerType,
 
 schema = {
     "app_content" : StructType([
-        StructField("appid",IntegerType(), True),
+        StructField("appid",StringType(), True),
         StructField("name",StringType(), True),
         StructField("type",StringType(), True),
-        StructField("require_age", StringType(), True),         # not number cause it's the age tag
-        StructField("dlc_list",ArrayType(IntegerType()),True),
+        StructField("required_age", StringType(), True),         # not number cause it's the age tag
+        StructField("dlc_list",ArrayType(StringType()),True),
         StructField(
             "price_overview",
             StructType([
                 StructField("currency",StringType(),True),
-                StructField("original_price",DoubleType(),True)
+                StructField("original_price",StringType(),True)
             ]),
             True
         ),
-        StructField("description",StringType(),True),
+        StructField("about_the_game",StringType(),True),
         StructField("pc_requirements",StringType(),True),
         StructField(
             "user_review",
             StructType([
-                StructField("reviewCount",IntegerType(),True),
-                StructField("ratingValue",IntegerType(),True),
-                StructField("bestRating",IntegerType(),True),
-                StructField("worstRating",IntegerType(),True),
+                StructField("reviewCount",StringType(),True),
+                StructField("ratingValue",StringType(),True),
+                StructField("bestRating",StringType(),True),
+                StructField("worstRating",StringType(),True),
                 StructField("status",StringType(),True)    
             ]),
             True
@@ -43,11 +43,39 @@ schema = {
         StructField("user_tags",ArrayType(StringType()),True)
     ]),
     "app_review" : StructType([
-        StructField("appid",IntegerType(),True),
-        StructField("reviews",ArrayType(StringType()),True)    
+        StructField("appid",StringType(),True),
+        StructField("reviews",
+            ArrayType(
+                StructType([
+                    StructField("recommendationid", StringType()),
+                    StructField("author", StructType([
+                        StructField("steamid", StringType()),
+                        StructField("num_games_owned", StringType()),
+                        StructField("num_reviews", StringType()),
+                        StructField("playtime_forever", StringType()),
+                        StructField("playtime_last_two_weeks", StringType()),
+                        StructField("playtime_at_review", StringType()),
+                        StructField("last_played", StringType()),
+                    ])),
+                    StructField("language", StringType()),
+                    StructField("review", StringType()),
+                    StructField("timestamp_created", StringType()),
+                    StructField("timestamp_updated", StringType()),
+                    StructField("voted_up", StringType()),
+                    StructField("votes_up", StringType()),
+                    StructField("votes_funny", StringType()),
+                    StructField("weighted_vote_score", StringType()),
+                    StructField("comment_count", StringType()),
+                    StructField("steam_purchase", StringType()),
+                    StructField("received_for_free", StringType()),
+                    StructField("written_during_early_access", StringType()),
+                    StructField("primarily_steam_deck", StringType())
+                ])
+            )
+        ,True)    
     ]),
     "app_tag" : StructType([
-        StructField("tagid",IntegerType(),True),
+        StructField("tagid",StringType(),True),
         StructField("name",StringType(),True)
     ])
 }
