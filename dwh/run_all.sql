1889a98f028afe78a3b0eb5d206ac954db8e53fa
phanlamthanhdu <root@duplt.localdomain>
2026-02-26 01:10:53 +0700

    Add SQL scripts to set up star schema in DWH

---
+++ dwh/run_all.sql
@@ -0,0 +1,8 @@
+\i /dwh/scripts/drop_all.sql
+\i /dwh/scripts/dim_game.sql
+\i /dwh/scripts/dim_review_text.sql
+\i /dwh/scripts/dim_date.sql
+\i /dwh/scripts/bridge_game_dlc.sql
+\i /dwh/scripts/bridge_game_production.sql
+\i /dwh/scripts/bridge_game_genre.sql
+\i /dwh/scripts/fact_review.sql
